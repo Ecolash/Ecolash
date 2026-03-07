090d09d15131bce05dd418bd7960b0ada3c63449
Tuhin Mondal <137093087+Ecolash@users.noreply.github.com>
2026-03-07 15:58:08 +0530

    Update fetchBentoUrl to handle errors and return null

--- getNewBento.ts
+++ getNewBento.ts
@@ -1,21 +1,24 @@
 const apiUrl = "https://opbento.vercel.app/api/bento?n=Tuhin%20Mondal&g=Ecolash&x=Tuhin_011&l=tuhin018&i=https%3A%2F%2Fraw.githubusercontent.com%2FEcolash%2FEcolash%2Frefs%2Fheads%2Fmain%2FImages%2Fbanner-pic.png&p=&z=2666d";
+
 interface BentoResponse {
   url: string;
 }
 
-const fetchBentoUrl = async (apiUrl: string): Promise<string> => {
+const fetchBentoUrl = async (apiUrl: string): Promise<string | null> => {
   try {
     const response = await fetch(apiUrl);
+
     if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
+      console.error(`HTTP error! status: ${response.status}`);
+      return null;
     }
-    const data: BentoResponse = (await response.json()) as BentoResponse;
+
+    const data: BentoResponse = await response.json();
     return data.url;
   } catch (error) {
     console.error("Error fetching Bento URL:", error);
-    throw error;
+    return null;
   }
 };
 
-// @ts-ignore
 fetchBentoUrl(apiUrl);
